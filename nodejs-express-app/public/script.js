46c26691934d4d1e57222c5f8af103313ee0b7bb
yunusibin33 <101434779+yunusibin33@users.noreply.github.com>
2023-11-02 17:56:14 +0300

    fdgdfg

---
+++ nodejs-express-app/public/script.js
@@ -0,0 +1,55 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Sayfa yüklendiğinde kişileri çek ve tabloyu güncelle
+    getPersonsAndPopulateTable();
+  });
+  
+  async function getPersonsAndPopulateTable() {
+    // Önce yerel depolamadan kişileri çek
+    const storedPersons = JSON.parse(localStorage.getItem('persons')) || [];
+  
+    // Tabloyu temizle
+    const tableBody = document.getElementById('personTable').getElementsByTagName('tbody')[0];
+    tableBody.innerHTML = '';
+  
+    // Yerel depolamadan çekilen kişileri tabloya ekle
+    storedPersons.forEach(person => {
+      addPersonToTable(person.firstName, person.lastName, person.imageData);
+    });
+  
+    // Sonra MongoDB'den kişileri çek ve tabloyu güncelle
+    const response = await fetch('http://localhost:3000/getPersons');
+    const persons = await response.json();
+  
+    // Kişileri yerel depolamada sakla
+    localStorage.setItem('persons', JSON.stringify(persons));
+  
+    // Kişileri tabloya ekle
+    persons.forEach(person => {
+      addPersonToTable(person.firstName, person.lastName, person.image.data);
+    });
+  }
+  // Kişiyi tabloya ekle
+  function addPersonToTable(firstName, lastName, imageData) {
+    const table = document.getElementById('personTable').getElementsByTagName('tbody')[0];
+    const newRow = table.insertRow(table.rows.length);
+    const cell1 = newRow.insertCell(0);
+    const cell2 = newRow.insertCell(1);
+    const cell3 = newRow.insertCell(2);
+  
+    const personInfoDiv = document.createElement('div');
+    personInfoDiv.classList.add('person-info');
+    const nameDiv = document.createElement('div');
+    nameDiv.innerHTML = `<strong>${firstName}</strong>`;
+    const lastNameDiv = document.createElement('div');
+    lastNameDiv.innerHTML = lastName;
+    const imageDiv = document.createElement('div');
+    imageDiv.innerHTML = `<img src="data:image/*;base64,${imageData.toString('base64')}" alt="${firstName} ${lastName}" class="img-thumbnail" onclick="openLightbox('data:image/*;base64,${imageData.toString('base64')}')">`;
+  
+    cell1.appendChild(personInfoDiv);
+    personInfoDiv.appendChild(nameDiv);
+    personInfoDiv.appendChild(lastNameDiv);
+    cell3.appendChild(imageDiv);
+  }
+  
+  // Diğer fonksiyonlar...
+  
--- nodejs-express-app/server.js
+++ nodejs-express-app/server.js
@@ -1,27 +1,47 @@
+// server.js dosyan
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path'); // Yeni eklenen satır
+const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 
-app.use(bodyParser.json());
+// MongoDB bağlantısı
+mongoose.connect('mongodb://localhost:27017/kisiDB', { useNewUrlParser: true, useUnifiedTopology: true });
 
-let users = [];
-
-app.post('/add-user', (req, res) => {
-  const newUser = req.body;
-  users.push(newUser);
-  res.json(newUser);
+// Veritabanı şeması
+const personSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  image: Buffer  // Resmi bir Buffer olarak saklıyoruz
 });
 
-app.get('/users', (req, res) => {
-  res.json(users);
-});
+// Veritabanı modeli
+const Person = mongoose.model('Person', personSchema);
 
-// Statik dosyaları servis etme
-app.use('/public', express.static(path.join(__dirname, 'public')));
+// Express middleware'leri
+app.use(express.json());
 
-app.listen(port, '0.0.0.0', () => {
-    console.log(`Server is running on http://localhost:${port}`);
+
+
+// Kişi ekleme endpoint'i
+app.post('/addPerson', async (req, res) => {
+  const { firstName, lastName, image } = req.body;
+
+  // MongoDB'ye kişiyi ekle
+  const newPerson = new Person({
+    firstName,
+    lastName,
+    image
   });
-  
+
+  try {
+    await newPerson.save();
+    res.send('Kişi başarıyla eklendi.');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Kişi eklenirken bir hata oluştu.');
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
+});
