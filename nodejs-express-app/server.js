78aa428432f9250b26c96822fa158acb12a37f39
yunusibin33 <101434779+yunusibin33@users.noreply.github.com>
2023-11-10 18:45:28 +0300

    sdfds

--- nodejs-express-app/server.js
+++ nodejs-express-app/server.js
@@ -1,89 +1,55 @@
-const mongoose = require('mongoose');
 const express = require('express');
-const fileUpload = require('express-fileupload');
+const mongoose = require('mongoose');
+const path = require('path'); // Eklenen satır
+
 const app = express();
 const port = 3000;
 
-app.use(fileUpload());
-mongoose.connect('mongodb://localhost:27017/kisiDB', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Veritabanı şeması
-const personSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String,
-  image: Buffer
-});
-
-// Veritabanı modeli
-const Person = mongoose.model('Person', personSchema);
-
-app.use(express.json());
-app.use(express.static('public'));
-
-app.post('/addPerson', async (req, res) => {
-  if (!req.files || Object.keys(req.files).length === 0) {
-    return res.status(400).send('Resim bulunamadı.');
-  }
-
-  const image = req.files.image;
-
-  // MongoDB'ye kişiyi ekle
-  const newPerson = new Person({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    image: image.data,
+// MongoDB bağlantısı
+mongoose.connect('mongodb://localhost:27017/kullanicilar', { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('MongoDB ile bağlantı sağlandı');
+  })
+  .catch((err) => {
+    console.error('MongoDB bağlantı hatası:', err);
   });
 
-  try {
-    await newPerson.save();
-    res.json({ message: 'Kişi başarıyla eklendi.' });
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Kişi eklenirken bir hata oluştu.');
-  }
+// Kullanıcı modeli
+const User = mongoose.model('User', {
+  firstName: String,
+  lastName: String,
 });
 
-// Kişileri getirme endpoint'i
-app.get('/getPersons', async (req, res) => {
+// JSON verileri okumak için middleware
+app.use(express.json());
+
+// Kullanıcı ekleme endpoint'i
+app.post('/api/newuser', async (req, res) => {
+  const { firstName, lastName } = req.body;
+
   try {
-    const persons = await Person.find();
-    res.json(persons);
-  } catch (err) {
-    console.error(err);
-    res.status(500).send('Kişiler getirilirken bir hata oluştu.');
-  }
-});
-
-async function getPersonsAndPopulateTable() {
-  try {
-    // Sonra MongoDB'den kişileri çek ve tabloyu güncelle
-    const response = await fetch('http://localhost:3000/getPersons');
-    const mongoPersons = await response.json();
-
-    // Yerel depolamadan kişileri çek
-    const storedPersons = JSON.parse(localStorage.getItem('persons')) || [];
-
-    // MongoDB'den çekilen kişileri yerel depolamadaki kişilere ekle
-    const allPersons = [...storedPersons, ...mongoPersons];
-
-    // Kişileri yerel depolamada sakla
-    localStorage.setItem('persons', JSON.stringify(allPersons));
-
-    // Tabloyu temizle
-    const tableBody = document.getElementById('personTable').getElementsByTagName('tbody')[0];
-    tableBody.innerHTML = '';
-
-    // Kişileri tabloya ekle
-    allPersons.forEach(person => {
-      addPersonToTable(person.firstName, person.lastName, person.image);
-    });
+    const user = new User({ firstName, lastName });
+    await user.save();
+    res.status(201).json(user);
   } catch (error) {
-    console.error('Hata:', error);
+    res.status(500).json({ error: 'Kullanıcı eklenirken bir hata oluştu.' });
   }
-}
+});
 
+// Tüm kullanıcıları listeleme endpoint'i
+app.get('/api/allusers', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: 'Kullanıcılar getirilirken bir hata oluştu.' });
+  }
+});
+
+// Public dizinindeki dosyaları servis etmek için
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Server'ı dinle
 app.listen(port, () => {
-  console.log(`Server listening at http://localhost:${port}`);
+  console.log(`Server http://localhost:${port} üzerinde çalışıyor`);
 });
