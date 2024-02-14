fb556136dfa659a522d8744e22b23687e0c17db4
yunusibin33 <101434779+yunusibin33@users.noreply.github.com>
2024-02-14 19:37:32 +0300

    mnvb

--- nodejs-express-app/start_pm2.bat
+++
@@ -1,6 +0,0 @@
-:: start_pm2.bat
-@echo off
-call npm config get prefix > prefix.txt
-set /p NODE_PATH=<prefix.txt
-set PM2_HOME=%NODE_PATH%\node_modules\pm2
-call %PM2_HOME%\pm2 start "C:\Users\Acer\Documents\GitHub\yunusibin.io\nodejs-express-app\server.js"
